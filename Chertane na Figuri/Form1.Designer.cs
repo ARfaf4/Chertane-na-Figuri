bf4259a8f7ac146d5a303cc7c1cff07fc52a0ca2
ARfaf4 <bosshero31@gmail.com>
2022-12-12 00:57:48 +0200

    FINAL(BONUS)

--- Chertane na Figuri/Form1.Designer.cs
+++ Chertane na Figuri/Form1.Designer.cs
@@ -32,13 +32,18 @@
             this.CircleBtn = new System.Windows.Forms.Button();
             this.RectangleBtn = new System.Windows.Forms.Button();
             this.StopBtn = new System.Windows.Forms.Button();
+            this.PolygonBtn = new System.Windows.Forms.Button();
+            this.SpamModeBtn = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EpilepticModeBtn = new System.Windows.Forms.Button();
+            this.ResetBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // TriangleBtn
             // 
-            this.TriangleBtn.Location = new System.Drawing.Point(62, 540);
+            this.TriangleBtn.Location = new System.Drawing.Point(12, 540);
             this.TriangleBtn.Name = "TriangleBtn";
-            this.TriangleBtn.Size = new System.Drawing.Size(106, 44);
+            this.TriangleBtn.Size = new System.Drawing.Size(64, 44);
             this.TriangleBtn.TabIndex = 0;
             this.TriangleBtn.Text = "Triangle";
             this.TriangleBtn.UseVisualStyleBackColor = true;
@@ -46,9 +51,9 @@
             // 
             // CircleBtn
             // 
-            this.CircleBtn.Location = new System.Drawing.Point(206, 540);
+            this.CircleBtn.Location = new System.Drawing.Point(93, 540);
             this.CircleBtn.Name = "CircleBtn";
-            this.CircleBtn.Size = new System.Drawing.Size(106, 44);
+            this.CircleBtn.Size = new System.Drawing.Size(75, 44);
             this.CircleBtn.TabIndex = 1;
             this.CircleBtn.Text = "Circle";
             this.CircleBtn.UseVisualStyleBackColor = true;
@@ -57,9 +62,9 @@
             // RectangleBtn
             // 
             this.RectangleBtn.AutoEllipsis = true;
-            this.RectangleBtn.Location = new System.Drawing.Point(353, 540);
+            this.RectangleBtn.Location = new System.Drawing.Point(184, 540);
             this.RectangleBtn.Name = "RectangleBtn";
-            this.RectangleBtn.Size = new System.Drawing.Size(106, 44);
+            this.RectangleBtn.Size = new System.Drawing.Size(75, 44);
             this.RectangleBtn.TabIndex = 2;
             this.RectangleBtn.Text = "Rectangle";
             this.RectangleBtn.UseVisualStyleBackColor = true;
@@ -67,19 +72,75 @@
             // 
             // StopBtn
             // 
-            this.StopBtn.Location = new System.Drawing.Point(500, 540);
+            this.StopBtn.Location = new System.Drawing.Point(368, 540);
             this.StopBtn.Name = "StopBtn";
-            this.StopBtn.Size = new System.Drawing.Size(106, 44);
+            this.StopBtn.Size = new System.Drawing.Size(75, 44);
             this.StopBtn.TabIndex = 3;
             this.StopBtn.Text = "Stop";
             this.StopBtn.UseVisualStyleBackColor = true;
             this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // PolygonBtn
+            // 
+            this.PolygonBtn.Location = new System.Drawing.Point(277, 540);
+            this.PolygonBtn.Name = "PolygonBtn";
+            this.PolygonBtn.Size = new System.Drawing.Size(75, 44);
+            this.PolygonBtn.TabIndex = 4;
+            this.PolygonBtn.Text = "Polygon (Glitched)";
+            this.PolygonBtn.UseVisualStyleBackColor = true;
+            this.PolygonBtn.Click += new System.EventHandler(this.PolygonBtn_Click);
+            // 
+            // SpamModeBtn
+            // 
+            this.SpamModeBtn.AutoSize = true;
+            this.SpamModeBtn.Location = new System.Drawing.Point(529, 30);
+            this.SpamModeBtn.Name = "SpamModeBtn";
+            this.SpamModeBtn.Size = new System.Drawing.Size(44, 17);
+            this.SpamModeBtn.TabIndex = 5;
+            this.SpamModeBtn.TabStop = true;
+            this.SpamModeBtn.Text = " ON";
+            this.SpamModeBtn.UseVisualStyleBackColor = true;
+            this.SpamModeBtn.CheckedChanged += new System.EventHandler(this.SpamModeBtn_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(526, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Spam mode:";
+            // 
+            // EpilepticModeBtn
+            // 
+            this.EpilepticModeBtn.Location = new System.Drawing.Point(553, 540);
+            this.EpilepticModeBtn.Name = "EpilepticModeBtn";
+            this.EpilepticModeBtn.Size = new System.Drawing.Size(75, 44);
+            this.EpilepticModeBtn.TabIndex = 8;
+            this.EpilepticModeBtn.Text = "Epileptic Mode";
+            this.EpilepticModeBtn.UseVisualStyleBackColor = true;
+            this.EpilepticModeBtn.Click += new System.EventHandler(this.EpilepticModeBtn_Click);
+            // 
+            // ResetBtn
+            // 
+            this.ResetBtn.Location = new System.Drawing.Point(460, 540);
+            this.ResetBtn.Name = "ResetBtn";
+            this.ResetBtn.Size = new System.Drawing.Size(75, 44);
+            this.ResetBtn.TabIndex = 9;
+            this.ResetBtn.Text = "Restart";
+            this.ResetBtn.UseVisualStyleBackColor = true;
+            this.ResetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(640, 617);
+            this.Controls.Add(this.ResetBtn);
+            this.Controls.Add(this.EpilepticModeBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SpamModeBtn);
+            this.Controls.Add(this.PolygonBtn);
             this.Controls.Add(this.StopBtn);
             this.Controls.Add(this.RectangleBtn);
             this.Controls.Add(this.CircleBtn);
@@ -87,6 +148,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -96,6 +158,11 @@
         private System.Windows.Forms.Button CircleBtn;
         private System.Windows.Forms.Button RectangleBtn;
         private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.Button PolygonBtn;
+        private System.Windows.Forms.RadioButton SpamModeBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button EpilepticModeBtn;
+        private System.Windows.Forms.Button ResetBtn;
     }
 }
 
--- Chertane na Figuri/Form1.cs
+++ Chertane na Figuri/Form1.cs
@@ -9,39 +9,147 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using System.Runtime.InteropServices;
+using System.Reflection;
 
 namespace Chertane_na_Figuri
 {
     public partial class Form1 : Form
     {
-        
+        private bool isspamon = false;
+        private bool istri = false;
+        private bool isrect = false;
+        private bool iscirc = false;
+        private bool ispoly = false;
+        private bool isepileptic = false;
+
         Random random = new Random();
+
         Thread thread;
+        Thread thread1;
+        Thread thread2;
+        Thread thread3;
+        Thread thread4;
+        Thread thread5;
         public Form1()
         {
             InitializeComponent();
         }      
         private void button1_Click(object sender, EventArgs e) //triugulnik
         {
-            thread = new Thread(DrawTriangle);
-            thread.Start();
+            istri = true;
+            if (isspamon)
+            {
+                thread = new Thread(DrawTriangle);
+                thread.Start();
+            }
+            else
+            {
+                thread1 = new Thread(DrawTriangle);
+                thread1.Start();
+            }
+            
         }
 
         private void button2_Click(object sender, EventArgs e) //krug
         {
-            thread = new Thread(DrawCircle);
-            thread.Start();
+            iscirc = true;
+            if (isspamon)
+            {
+                thread = new Thread(DrawCircle);
+                thread.Start();
+            }
+            else
+            {
+                thread2 = new Thread(DrawCircle);
+                thread2.Start(); ;
+            }
         }
 
         private void RectangleBtn_Click(object sender, EventArgs e) //pravougulnik
-        {            
-            thread = new Thread(DrawRectangle);
-            thread.Start();
+        {
+            isrect = true;
+            if (isspamon)
+            {
+                thread = new Thread(DrawRectangle);
+                thread.Start();
+            }
+            else
+            {
+                thread3 = new Thread(DrawRectangle);
+                thread3.Start();
+            }
+        }
+        private void PolygonBtn_Click(object sender, EventArgs e)
+        {
+            ispoly = true;
+            if (isspamon)
+            {
+                thread = new Thread(DrawPolygon);
+                thread.Start();
+            }
+            else
+            {
+                thread4 = new Thread(DrawPolygon);
+                thread4.Start();
+            }
         }
         private void StopBtn_Click(object sender, EventArgs e)
         {
-            thread.Abort(true);
-            MessageBox.Show("Thread have been stopped");
+            if (isspamon)
+            {
+                thread.Abort(true);
+                MessageBox.Show("Thread have been stopped!");
+                MessageBox.Show("Nope it wasnt!");
+            }
+            else
+            {
+                if (istri)
+                {
+                    thread1.Abort(true);
+                }
+                if (iscirc)
+                {
+                    thread2.Abort(true);
+                }
+                if (isrect)
+                {
+                    thread3.Abort(true);
+                }
+                if (ispoly)
+                {
+                    thread4.Abort(true);
+                }
+                if (isepileptic)
+                {
+                    thread5.Abort(true);
+                }
+                MessageBox.Show("Threads have been stopped!");
+            }
+            
+        }
+        private void SpamModeBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            isspamon = true;
+        }
+
+        private void EpilepticModeBtn_Click(object sender, EventArgs e)
+        {
+            ispoly = true;
+            if (isepileptic)
+            {
+                thread = new Thread(EpilepticMode);
+                thread.Start();
+            }
+            else
+            {
+                thread5 = new Thread(EpilepticMode);
+                thread5.Start();
+            }
+        }
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+            Environment.Exit(0);
         }
         private void DrawRectangle()
         {
@@ -53,48 +161,80 @@ namespace Chertane_na_Figuri
                     int b = new Random().Next(1, 400);
                     if (x + a <= 600 && y + b <= 600)
                     {
-                        Color color = Color.FromArgb(random.Next(0,255), random.Next(0,255), random.Next(0,255));
+                        Color color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
                         Pen Pen = new Pen(color);
                         Pen.Width = 5;
                         this.CreateGraphics().DrawRectangle(Pen, x, y, a, b);
                         Thread.Sleep(3000);
-                    }                  
-                }           
+                    }
+                }                    
         }
         private void DrawCircle()
         {
-            for (int i = 0; i < 50; i--)
-            {
-                int x = new Random().Next(1, 656);
-                int y = new Random().Next(1, 656);
-                int r = new Random().Next(1, 400);
-                if (r + x <= 656 && r + y <= 600)
+                for (int i = 0; i < 50; i--)
+                {
+                    int x = new Random().Next(1, 656);
+                    int y = new Random().Next(1, 656);
+                    int r = new Random().Next(1, 400);
+                    if (r + x <= 656 && r + y <= 600)
+                    {
+                        Color color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                        Pen Pen = new Pen(color);
+                        Pen.Width = 5;
+                        this.CreateGraphics().DrawEllipse(Pen, x, y, r, r);
+                        Thread.Sleep(4000);
+                    }
+                }           
+        }
+        private void DrawTriangle()
+        {
+                for (int i = 0; i < 50; i--)
                 {
                     Color color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
                     Pen Pen = new Pen(color);
                     Pen.Width = 5;
-                    this.CreateGraphics().DrawEllipse(Pen, x, y, r, r);
-                    Thread.Sleep(4000);
-                }
-            }          
+                    Point point = new Point(random.Next(1, 606), random.Next(1, 606));
+                    Point point1 = new Point(random.Next(1, 606), random.Next(1, 606));
+                    Point point2 = new Point(random.Next(1, 606), random.Next(1, 606));
+                    this.CreateGraphics().DrawLine(Pen, point, point1);
+                    this.CreateGraphics().DrawLine(Pen, point, point2);
+                    this.CreateGraphics().DrawLine(Pen, point1, point2);
+                    Thread.Sleep(2000);
+                }           
         }
-        private void DrawTriangle()
+        private void DrawPolygon()
         {
-            for (int i = 0; i < 50; i--)
-            {
-                Color color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
-                Pen Pen = new Pen(color);
-                Pen.Width = 5;
-                Point point = new Point(random.Next(1, 606), random.Next(1, 606));
-                Point point1 = new Point(random.Next(1, 606), random.Next(1, 606));
-                Point point2 = new Point(random.Next(1, 606), random.Next(1, 606));
-                this.CreateGraphics().DrawLine(Pen, point, point1);
-                this.CreateGraphics().DrawLine(Pen, point, point2);
-                this.CreateGraphics().DrawLine(Pen, point1, point2);
-                Thread.Sleep(2000);
-            }
-            
+                for (int i = 0; i < 50; i--)
+                {
+                    int n = new Random().Next(5, 9);
+                    Point[] points = new Point[n];
+                    Color color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                    Pen Pen = new Pen(color);
+                    Pen.Width = 5;
+                    for (int ii = 0; ii < n; ii++)
+                    {
+                        Point pt = new Point(random.Next(1, 606), random.Next(1, 606));
+                        points[ii] = pt;
+                    }
+                    this.CreateGraphics().DrawPolygon(Pen, points);
+                    Thread.Sleep(1500);
+                }   
         }
-
+       
+        private void EpilepticMode()
+        {
+                for (int i = 0; i < 50; i--)
+                {
+                    Color color = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                    Pen Pen = new Pen(color);
+                    Pen.Width = 5;
+                    Point point = new Point(random.Next(1, 606), random.Next(1, 606));
+                    Point point1 = new Point(random.Next(1, 606), random.Next(1, 606));
+                    Point point2 = new Point(random.Next(1, 606), random.Next(1, 606));
+                    this.CreateGraphics().DrawLine(Pen, point, point1);
+                    this.CreateGraphics().DrawLine(Pen, point, point2);
+                    this.CreateGraphics().DrawLine(Pen, point1, point2);
+                }      
+        }
     }
 }
