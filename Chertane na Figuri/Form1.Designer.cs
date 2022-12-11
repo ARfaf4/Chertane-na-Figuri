19a5a690aa6362d3057aad31c8272297d44020a6
ARfaf4 <bosshero31@gmail.com>
2022-12-11 16:57:00 +0200

    Interface

--- Chertane na Figuri/Form1.Designer.cs
+++ Chertane na Figuri/Form1.Designer.cs
@@ -28,37 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.TriangleBtn = new System.Windows.Forms.Button();
+            this.CircleBtn = new System.Windows.Forms.Button();
+            this.RectangleBtn = new System.Windows.Forms.Button();
+            this.BonusBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // TriangleBtn
             // 
-            this.button1.Location = new System.Drawing.Point(108, 83);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 44);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.TriangleBtn.Location = new System.Drawing.Point(59, 337);
+            this.TriangleBtn.Name = "TriangleBtn";
+            this.TriangleBtn.Size = new System.Drawing.Size(106, 44);
+            this.TriangleBtn.TabIndex = 0;
+            this.TriangleBtn.Text = "Triangle";
+            this.TriangleBtn.UseVisualStyleBackColor = true;
+            this.TriangleBtn.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // CircleBtn
             // 
-            this.button2.Location = new System.Drawing.Point(397, 103);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.CircleBtn.Location = new System.Drawing.Point(208, 337);
+            this.CircleBtn.Name = "CircleBtn";
+            this.CircleBtn.Size = new System.Drawing.Size(106, 44);
+            this.CircleBtn.TabIndex = 1;
+            this.CircleBtn.Text = "Circle";
+            this.CircleBtn.UseVisualStyleBackColor = true;
+            this.CircleBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // RectangleBtn
+            // 
+            this.RectangleBtn.AutoEllipsis = true;
+            this.RectangleBtn.Location = new System.Drawing.Point(359, 337);
+            this.RectangleBtn.Name = "RectangleBtn";
+            this.RectangleBtn.Size = new System.Drawing.Size(106, 44);
+            this.RectangleBtn.TabIndex = 2;
+            this.RectangleBtn.Text = "Rectangle";
+            this.RectangleBtn.UseVisualStyleBackColor = true;
+            this.RectangleBtn.Click += new System.EventHandler(this.RectangleBtn_Click);
+            // 
+            // BonusBtn
+            // 
+            this.BonusBtn.Location = new System.Drawing.Point(501, 337);
+            this.BonusBtn.Name = "BonusBtn";
+            this.BonusBtn.Size = new System.Drawing.Size(106, 44);
+            this.BonusBtn.TabIndex = 3;
+            this.BonusBtn.Text = "Bonus";
+            this.BonusBtn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(640, 428);
+            this.Controls.Add(this.BonusBtn);
+            this.Controls.Add(this.RectangleBtn);
+            this.Controls.Add(this.CircleBtn);
+            this.Controls.Add(this.TriangleBtn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -67,8 +91,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button TriangleBtn;
+        private System.Windows.Forms.Button CircleBtn;
+        private System.Windows.Forms.Button RectangleBtn;
+        private System.Windows.Forms.Button BonusBtn;
     }
 }
 
